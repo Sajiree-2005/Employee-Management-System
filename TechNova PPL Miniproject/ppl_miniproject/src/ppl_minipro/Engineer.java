6f3c4ec98d8f2fb1b939986de869bc470608a8bc
Sajiree <sajireedamle@gmail.com>
2026-04-15 22:54:38 +0530

    Delete TechNova PPL Miniproject directory

--- TechNova PPL Miniproject/ppl_miniproject/src/ppl_minipro/Engineer.java
+++
@@ -1,7 +0,0 @@
-package ppl_minipro;
-
-public class Engineer extends Employee {
-    public Engineer(String name, int id, double salary, String domain) {
-        super(name, id, salary, domain);
-    }
-}
